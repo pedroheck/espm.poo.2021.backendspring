7479579c519a2c37016a813b5eadc14efefc4f37
pedroheck <pedroscheck@hotmail.com>
2021-05-27 11:04:42 -0300

    First version

---
+++ src/test/java/br/espm/poo/springAula/backendspring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.espm.poo.springAula.backendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
